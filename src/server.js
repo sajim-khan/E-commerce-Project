e3dda83faa83a43cda1a3fe22ef864cc06cc8eb4
sajim-khan <shossain1427@gmail.com>
2023-07-18 23:45:44 +0600

    added middleware

--- src/server.js
+++ src/server.js
@@ -3,12 +3,9 @@ const morgan = require('morgan')
 const app = express();
 
 app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-// app.get("/", (req, res) => {
-//     res.status(200).send({
-//       message: "welcome to server",
-//     });
-// })
 
 app.get("/products", (req, res) => {
   res.status(200).send({
